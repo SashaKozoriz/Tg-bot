61637c2880601324011dd81c4b078dcbdc63b850
Sasha Kozoriz <sashakozoriz2012@gmail.com>
2023-04-20 01:32:10 +0300

    add qoutes generator

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const telegramApi = require("node-telegram-bot-api");
 
-const {gameOptions, againOptions} = require('./options')
+const { gameOptions, againOptions } = require("./options");
 
 const token = "5435678116:AAFVFwYBKGxkaEZ2m_cJDnBevXQN2gvQHiA";
 
@@ -8,26 +8,41 @@ const bot = new telegramApi(token, { polling: true });
 
 const chats = {};
 
+const qoutes = require('./qoutes');
 
 const startGame = async (chatId) => {
   await bot.sendMessage(
     chatId,
     "Зараз я загадаю цифру від 1 до 9 а ти маєш відгадати її"
   );
-  const randomNumber = Math.floor(Math.random() * 10);
+  const randomNumber = Math.floor(Math.random() * 9) + 1;
   chats[chatId] = randomNumber;
   await bot.sendMessage(chatId, "Відгадуй", gameOptions);
 };
+
+
+const keyboard = {
+  resize_keyboard: true,
+  keyboard: [[{ text: "Надіслати локацію", request_location: true }]],
+};
+
+bot.onText(/\/location/, (msg) => {
+  bot.sendMessage(msg.chat.id, "Поділись локацією зі мною", {
+    reply_markup: keyboard,
+  });
+});
 
 const start = () => {
   bot.on("message", async (msq) => {
     const text = msq.text;
     const chatId = msq.chat.id;
-
+    console.log(msq);
     bot.setMyCommands([
       { command: "/start", description: "Привітання" },
       { command: "/info", description: "Інформація про користувача" },
       { command: "/game", description: "Зіграти в гру" },
+      { command: "/qoutes", description: "Випадкові цитати" },
+      { command: "/location", description: "Надіслати локацію" },
     ]);
 
     if (text === "/start") {
@@ -47,6 +62,24 @@ const start = () => {
 
     if (text === "/game") {
       return startGame(chatId);
+    }
+
+    if (text === "/qoutes") {
+      return bot.sendMessage(
+        chatId,
+        qoutes[Math.floor(Math.random() * qoutes.length)]
+      );
+    }
+
+    if (text === "/location") {
+      return bot.sendMessage(chatId,"")
+    }
+
+    if (msq.location) {
+      const lat = msq.location.latitude;
+      const lon = msq.location.longitude;
+      bot.sendMessage(chatId, `Твоє місцезнаходження ${lat}, ${lon}`);
+      return start
     }
 
     return bot.sendMessage(chatId, "Я тебе не зрозумів");
